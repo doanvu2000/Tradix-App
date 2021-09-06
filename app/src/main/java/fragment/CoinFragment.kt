071def890545c3097882b2759f17bd26d5fd1a5c
doanvu2000 <doanvu20122000@gmail.com>
2021-09-06 12:03:35 +0700

    update animation

--- app/src/main/java/fragment/CoinFragment.kt
+++ app/src/main/java/fragment/CoinFragment.kt
@@ -1,17 +1,15 @@
 package fragment
 
-import `object`.User
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.tradixapp.R
 import kotlinx.android.synthetic.main.coin.view.*
-import kotlinx.android.synthetic.main.item_layout_rcv_list_coin.view.*
-import kotlinx.android.synthetic.main.item_layout_rcv_list_coin.view.tvUserName
 
 class CoinFragment : Fragment() {
     var isClickBell = false
@@ -23,14 +21,19 @@ class CoinFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.coin, container, false)
         Log.d("abc", "onCreateView: ${args.userCoin}}")
-        view.tvCoinUserName.text = args.userCoin?.userName
-        view.tvCoinCurrentMoney.text = args.userCoin?.currentMoney
-        view.tvCoinCurrentPercent.text = args.userCoin?.currentPercent
+        if (args.userCoin != null) {
+            view.tvCoinUserName.text = args.userCoin?.userName
+            view.tvCoinCurrentMoney.text = args.userCoin?.currentMoney
+            view.tvCoinCurrentPercent.text = args.userCoin?.currentPercent
+        }
         view.imgBellCoin.setOnClickListener {
             isClickBell = !isClickBell
             if (isClickBell) {
                 view.imgBellCoin.setBackgroundResource(R.drawable.bell_blue)
             } else view.imgBellCoin.setBackgroundResource(R.drawable.bell_black)
+        }
+        view.btnBackToHomeInCoin.setOnClickListener {
+            findNavController().navigateUp()
         }
         return view
     }
